35b574f22427839e75400850c7da49bc66f287f2
Neil Brown <neilb@suse.de>
2006-05-22 09:33:29 +1000

    Cope if 'best' doesn't find anything any good.

--- bestmatch.c
+++ bestmatch.c
@@ -348,7 +348,8 @@ void remap(struct best *best, int cnt,
 
 	if (a1.elcnt == 0 && a2.elcnt == 0) return;
 
-	for (b=1; b<cnt; b++) {
+	for (b=1; b<cnt; b++)
+	   if (best[b].val>0) {
 #if 0
 		printf("best %d,%d  %d,%d\n",
 		       best[b].xlo,best[b].ylo,
