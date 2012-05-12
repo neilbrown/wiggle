db182d650d14481d81d3dc56f18c83ff75bfbf48
Neil Brown <neilb@suse.de>
2012-05-12 15:37:08 +1000

    Add endian.h back to config.h
    It turns out we do need it after all - but only include it if we
    really do need it.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- config.h
+++ config.h
@@ -1,6 +1,9 @@
 
 /* Includes and defines for ccan files */
 
+#if !defined(LITTLE_ENDIAN) && !defined(BIG_ENDIAN)
+ #include <endian.h>
+#endif
 #ifdef LITTLE_ENDIAN
  #define HAVE_LITTLE_ENDIAN 1
  #define HAVE_BIG_ENDIAN 0
