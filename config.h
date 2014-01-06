3a1ab01be4af79136081f4977b3eaa6ff3eb0d3e
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2014-01-06 03:57:03 +0000

    Fix endian checks as per austin group definitions of endian.h
    Debian-Bug: http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=733831
    Reference: http://www.opengroup.org/austin/docs/austin_514.txt
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- config.h
+++ config.h
@@ -1,17 +1,17 @@
 
 /* Includes and defines for ccan files */
 
-#if !defined(LITTLE_ENDIAN) && !defined(BIG_ENDIAN)
+#if !defined(BYTE_ORDER) && !defined(LITTLE_ENDIAN) && !defined(BIG_ENDIAN)
  #if defined(__APPLE__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
   #include <machine/endian.h>
  #else
   #include <endian.h>
  #endif
 #endif
-#ifdef LITTLE_ENDIAN
+#if BYTE_ORDER == LITTLE_ENDIAN
  #define HAVE_LITTLE_ENDIAN 1
  #define HAVE_BIG_ENDIAN 0
-#elif defined(BIG_ENDIAN)
+#elif BYTE_ORDER == BIG_ENDIAN
  #define HAVE_LITTLE_ENDIAN 0
  #define HAVE_BIG_ENDIAN 1
 #else
